8429c514447ed7308fb07dc05382430807c9ae28
Shubhangi Verma <shubhangi.verma.1408@gmail.com>
2017-12-25 19:08:50 +0530

    added updated program code after adding record feature

--- Beat_Box/app/src/main/java/com/example/shubhangi/beat_box/MainActivity.java
+++ Beat_Box/app/src/main/java/com/example/shubhangi/beat_box/MainActivity.java
@@ -1,42 +1,57 @@
 package com.example.shubhangi.beat_box;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
+import android.view.View.OnClickListener;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
-
+Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btn = findViewById(R.id.PlayButton);
-        Button btn2 = findViewById(R.id.EditButton);
-        ImageButton RecordAudio = findViewById(R.id.RecordButton);
-        RecordAudio.setOnClickListener(new View.OnClickListener() {
+        addListenerOnButton();
+        ImageButton TrimAudio = findViewById(R.id.TrimButton);
+        ImageButton MergeAudio = findViewById(R.id.MergeButton);
+
+        TrimAudio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Click to record audio!", Toast.LENGTH_SHORT)
-                        .show();
+                Toast.makeText(getApplicationContext(), "Click to trim audio!", Toast.LENGTH_LONG);
+            }
+        });
+        MergeAudio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Click to merge audio!", Toast.LENGTH_LONG);
             }
         });
 
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Click to edit audio!", Toast.LENGTH_SHORT)
-                        .show();
-            }
-        });
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Click to play audio!", Toast.LENGTH_SHORT)
-                        .show();
-            }
-        });
     }
+
+    public void addListenerOnButton() {
+
+        ImageButton RecordAudio = findViewById(R.id.RecordButton);
+
+        RecordAudio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               startActivity(new Intent(MainActivity.this, DisplayActivity.class) );
+
+            }
+        });
+
+
+        }
 }
+
+
